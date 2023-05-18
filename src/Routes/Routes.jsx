3594b0a5502a3fc5d64737e2d1a185e35d096c5a
sanaullahsabere <you@example.com>
2023-05-18 12:46:02 +0600

    error page done

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    errorElement: <ErrorPage></ErrorPage>,
     element: <Main></Main>,
+    children: [
+      
+    ]
   },
 ]);
 
--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React from "react";
+import errorLottie from "../../assets/error/90344-spider-404.json";
+import Lottie from "lottie-react";
+import { Link, useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
+  const { error, status } = useRouteError();
+
   return (
-    <div>
-      
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="max-w-md text-center">
+        <Lottie
+          className="mx-auto"
+          animationData={errorLottie}
+          loop={true}
+        />
+        <h1 className="text-4xl font-bold mb-4">Oops!</h1>
+        <h2 className="mb-8 font-extrabold text-9xl text-rose-700">
+          <span className="sr-only">Error</span>
+          {status || 404}
+        </h2>
+        <p className="text-2xl font-semibold md:text-3xl text-red-800 mb-8">
+          {error?.message}
+        </p>
+        <Link to="/">
+          <button className="btn bg-rose-700 text-white font-semibold py-2 px-4 rounded-md hover:bg-rose-800">
+            Go back to Home
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
