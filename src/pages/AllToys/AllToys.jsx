728864c7da44c3457f92aae565cf5f41e77f1c96
sanaullahsabere <you@example.com>
2023-05-20 23:57:44 +0600

    all toy limit 20 done

--- src/pages/AllToys/AllToys.jsx
+++ src/pages/AllToys/AllToys.jsx
@@ -6,13 +6,25 @@ const AllToys = () => {
   
   const [bookings, setBookings] = useState([]);
 
+  const [showAll, setShowAll] = useState(false);
+
   const url = `http://localhost:5000/bookings`;
   useEffect(() => {
     fetch(url)
       .then(res => res.json())
     .then(data => setBookings(data))
   },[])
+// now star to update if you feel any bad then just go back with ctrl+z
+  
+   const handleShowAll = () => {
+     setShowAll(true);
+   };
 
+   const handleShowLess = () => {
+     setShowAll(false);
+   };
+
+  
   return (
     <div className="max-w-7xl mx-auto my-10">
       <h2 className="text-2xl">All Toys:{bookings.length}</h2>
@@ -35,12 +47,38 @@ const AllToys = () => {
             </tr>
           </thead>
           <tbody>
-            {/* row 1 */}
+            {/* row 1
             {bookings.map((booking) => (
               <AllRow key={booking._id} booking={booking}></AllRow>
-            ))}
+            ))} */}
+
+            {/* Rows */}
+            {bookings
+              .slice(0, showAll ? bookings.length : 20)
+              .map((booking) => (
+                <AllRow key={booking._id} booking={booking}></AllRow>
+              ))}
           </tbody>
         </table>
+      </div>
+      <div className="text-center">
+        {!showAll && bookings.length > 20 && (
+          <button
+            onClick={handleShowAll}
+            className="my-4 px-4 py-2  hover:bg-zinc-500 hover:text-black bg-rose-600 text-white rounded"
+          >
+            Show All
+          </button>
+        )}
+
+        {showAll && (
+          <button
+            onClick={handleShowLess}
+            className="my-4 px-4 py-2  hover:bg-zinc-500 hover:text-black bg-rose-600 text-white rounded"
+          >
+            Show Less
+          </button>
+        )}
       </div>
     </div>
   );
